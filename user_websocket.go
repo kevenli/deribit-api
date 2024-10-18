f24a275e17a88b0e28d4fe5ae169138d52f686f7
kevenli <pbleester@gmail.com>
2024-10-18 08:39:44 +0000

    user_websocket SubscribeUserOrders

--- user_websocket.go
+++ user_websocket.go
@@ -4,19 +4,59 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"log"
+	"strings"
+	"sync"
 	"time"
 
+	"github.com/chuckpreslar/emission"
 	"github.com/frankrap/deribit-api/models"
 	"github.com/sourcegraph/jsonrpc2"
 	"nhooyr.io/websocket"
 )
 
+type UserOrdersEventCallback func(*models.Order)
+
+// type UserWebsocketCallbacks struct {
+// 	userOrdersCallback UserOrdersEventCallback
+// }
+
 type UserWebsocket struct {
-	ctx       context.Context
-	addr      string
-	apiKey    string
-	secretKey string
-	conn      *websocket.Conn
+	ctx         context.Context
+	addr        string
+	apiKey      string
+	secretKey   string
+	conn        *websocket.Conn
+	isConnected bool
+	mu          sync.RWMutex
+	// callbacks   UserWebsocketCallbacks
+	emitter *emission.Emitter
+}
+
+func NewUserWebsocket(cfg *Configuration) (*UserWebsocket, error) {
+	ctx := cfg.Ctx
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	websocket := &UserWebsocket{
+		ctx:       ctx,
+		addr:      cfg.Addr,
+		apiKey:    cfg.ApiKey,
+		secretKey: cfg.SecretKey,
+		emitter:   emission.NewEmitter(),
+	}
+	err := websocket.connect()
+	if err != nil {
+		return nil, err
+	}
+
+	err = websocket.authenticate()
+	if err != nil {
+		return nil, err
+	}
+
+	return websocket, nil
 }
 
 func (u *UserWebsocket) connect() error {
@@ -32,6 +72,7 @@ func (u *UserWebsocket) connect() error {
 	if err != nil {
 		return err
 	}
+	u.setIsConnected(true)
 	return nil
 }
 
@@ -77,12 +118,18 @@ func (u *UserWebsocket) authenticate() error {
 	return nil
 }
 
+// Get UserWebsocket object from an existing client, which is also assured connected.
+// This method is not recommanded now, because Client has already established
+// a websocket connection.
+// This is useful when Client only provides restful apis, and user want
+// more features about streaming.
 func (c *Client) ConnectUserWebsocket() (*UserWebsocket, error) {
 	websocket := UserWebsocket{
 		ctx:       c.ctx,
 		addr:      c.addr,
 		apiKey:    c.apiKey,
 		secretKey: c.secretKey,
+		emitter:   emission.NewEmitter(),
 	}
 	err := websocket.connect()
 	if err != nil {
@@ -90,4 +137,103 @@ func (c *Client) ConnectUserWebsocket() (*UserWebsocket, error) {
 	}
 
 	return &websocket, nil
+}
+
+// setIsConnected sets state for isConnected
+func (u *UserWebsocket) setIsConnected(state bool) {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+
+	u.isConnected = state
+}
+
+// IsConnected returns the WebSocket connection state
+func (u *UserWebsocket) IsConnected() bool {
+	u.mu.RLock()
+	defer u.mu.RUnlock()
+
+	return u.isConnected
+}
+
+func (u *UserWebsocket) SubscribeUserOrders(kind string, currency string, callback UserOrdersEventCallback) error {
+	request := jsonrpc2.Request{
+		ID:     jsonrpc2.ID{Num: 1},
+		Method: "/private/subscribe",
+	}
+
+	channel := fmt.Sprintf("user.orders.%s.%s.raw", kind, currency)
+	params := models.SubscribeParams{}
+	params.Channels = []string{channel}
+
+	request.SetParams(params)
+	sendData, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+
+	err = u.conn.Write(u.ctx, websocket.MessageText, sendData)
+	if err != nil {
+		return err
+	}
+
+	// u.callbacks.userOrdersCallback = callback
+	u.emitter.On(channel, callback)
+	return nil
+}
+
+// This method will loop receiving events until an error occured.
+func (u *UserWebsocket) StartConsumeEvents() error {
+	for {
+		response := jsonrpc2.Response{}
+		event := Event{}
+
+		_, recvData, err := u.conn.Read(u.ctx)
+		if err != nil {
+			return err
+		}
+
+		// jsonrpc downstream message will be either a Response or
+		// a Notification(as structure of Ruquest)
+		// see: https://www.jsonrpc.org/specification
+		// detect if it is a Response
+		if err := json.Unmarshal(recvData, &response); err != nil {
+			return err
+		}
+
+		if response.ID.Num != 0 || response.ID.Str != "" {
+			// No id value, data is not a response.
+			log.Printf("Response received, %v", response)
+
+			if response.Error != nil {
+				return response.Error
+			}
+			continue
+		}
+
+		// notification data should be a Request.
+		request := jsonrpc2.Request{}
+		json.Unmarshal(recvData, &request)
+		if request.Method != "subscription" {
+			log.Printf("non-subscription event received, %v", request)
+			continue
+		}
+
+		if err := json.Unmarshal(*request.Params, &event); err != nil {
+			return err
+		}
+
+		if strings.HasPrefix(event.Channel, "user.orders") {
+			order := models.Order{}
+			if err := json.Unmarshal(event.Data, &order); err != nil {
+				return err
+			}
+
+			// if u.callbacks.userOrdersCallback != nil {
+			// 	u.callbacks.userOrdersCallback(&order)
+			// }
+			u.emitter.Emit(event.Channel, &order)
+		} else {
+			log.Printf("Unknown event %s", event.Channel)
+		}
+	}
 }
--- user_websocket_test.go
+++ user_websocket_test.go
@@ -1,10 +1,14 @@
 package deribit
 
 import (
+	"os"
+	"sync"
 	"testing"
+
+	"github.com/frankrap/deribit-api/models"
 )
 
-func TestConnectUserWebsocket(t *testing.T) {
+func getConfig() *Configuration {
 	cfg := &Configuration{
 		Addr:          TestBaseURL,
 		ApiKey:        "AsJTU16U",
@@ -12,24 +16,119 @@ func TestConnectUserWebsocket(t *testing.T) {
 		AutoReconnect: true,
 		DebugMode:     true,
 	}
+
+	apiKey := os.Getenv("DERIBIT_API_KEY")
+	if apiKey != "" {
+		cfg.ApiKey = apiKey
+	}
+
+	apiSecret := os.Getenv("DERIBIT_API_SECRET")
+	if apiSecret != "" {
+		cfg.SecretKey = apiSecret
+	}
+
+	return cfg
+}
+
+func TestConnectUserWebsocket(t *testing.T) {
+	cfg := getConfig()
+	websocket, err := NewUserWebsocket(cfg)
+	if err != nil {
+		t.Fatalf("%s, %v", err, websocket)
+	}
+
+	if got, expect := websocket.IsConnected(), true; expect != got {
+		t.Errorf("UserWebsocket.IsConnected, got %v != expect %v", got, expect)
+	}
+}
+
+func TestClientConnectUserWebsocket(t *testing.T) {
+	cfg := getConfig()
 	client := New(cfg)
 	websocket, err := client.ConnectUserWebsocket()
 	if err != nil {
 		t.Fatalf("%s, %v", err, websocket)
 	}
+
+	if got, expect := websocket.IsConnected(), true; expect != got {
+		t.Errorf("UserWebsocket.IsConnected, got %v != expect %v", got, expect)
+	}
 }
 
 func TestConnectUserWebsocketInvalidKey(t *testing.T) {
-	cfg := &Configuration{
-		Addr:          TestBaseURL,
-		ApiKey:        "AsJTU16U",
-		SecretKey:     "xxx",
-		AutoReconnect: true,
-		DebugMode:     true,
-	}
+	cfg := getConfig()
+	cfg.SecretKey = "SomeWrongKey"
 	client := New(cfg)
 	_, err := client.ConnectUserWebsocket()
 	if err == nil || err.Error() != "Authenticate Failed" {
 		t.Fatalf("Should catch error here.")
 	}
 }
+
+func TestUserOrderEventSubscription(t *testing.T) {
+	cfg := getConfig()
+	client := New(cfg)
+	websocket, err := client.ConnectUserWebsocket()
+
+	if err != nil {
+		t.Fatalf("%s, %v", err, websocket)
+	}
+
+	// wait event, notify when any UserOrder event received.
+	wg := &sync.WaitGroup{}
+
+	// subscribe on non-existing currency will not get an error
+	websocket.SubscribeUserOrders("any", "any", func(o *models.Order) {
+		t.Logf("Order event: %v", o)
+		wg.Done()
+	})
+
+	go websocket.StartConsumeEvents()
+
+	params := &models.BuyParams{
+		InstrumentName: "BTC-PERPETUAL",
+		Amount:         10,
+		Price:          24000,
+		Type:           "limit",
+	}
+
+	wg.Add(1)
+	result, err := client.Buy(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("OrderResponse: %v", result)
+
+	// cancel order, ignore code 11020 max_open_orders_per_instrument error
+	orderId := result.Order.OrderID
+	cancelResult, err := client.Cancel(&models.CancelParams{OrderID: orderId})
+	if err != nil {
+		t.Fatal(err, cancelResult)
+	}
+
+	wg.Wait()
+}
+
+// func TestWrongUserOrdersSubscription(t *testing.T) {
+// 	cfg := getConfig()
+// 	client := New(cfg)
+// 	websocket, err := client.ConnectUserWebsocket()
+
+// 	if err != nil {
+// 		t.Fatalf("%s, %v", err, websocket)
+// 	}
+
+// 	// subscribe on non-existing currency will not get an error
+// 	ch := make(chan *models.Order)
+// 	websocket.SubscribeUserOrders("any", "wrong_currency", func(o *models.Order) {
+// 		t.Logf("Order event: %v", o)
+// 		ch <- o
+// 	})
+
+// 	err = websocket.StartConsumeEvents()
+// 	if err != nil {
+// 		t.Fatalf("Subscription error not caught")
+// 	}
+
+// 	<-ch
+// }
